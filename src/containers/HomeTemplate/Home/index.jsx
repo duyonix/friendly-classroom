7266a9be65f7ef43477e4acefc6c4d9d2d70feb7
DuyOnix <dtduy2210@gmail.com>
2021-11-26 00:55:29 +0700

    use mui

--- src/containers/HomeTemplate/Home/index.jsx
+++ src/containers/HomeTemplate/Home/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 function Home(props) {
-  return <div>Home Page</div>;
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
 }
 
 export default Home;
