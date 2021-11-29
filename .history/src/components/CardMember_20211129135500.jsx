9d7ea028f52221228e846df597cfb62b54e0ea27
Võ Hoàng Vũ <vuprotk21@gmail.com>
2021-11-29 15:36:00 +0700

    hoangVu done intro page

---
+++ .history/src/components/CardMember_20211129135500.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function CardMember(props) {
+  const student = props.student;
+  console.log(student);
+  return <div className="card-member"></div>;
+}
