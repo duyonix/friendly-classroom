410814e59d899089a89e8e711ac007615888f21b
Võ Hoàng Vũ <vuprotk21@gmail.com>
2021-12-24 09:39:49 +0700

    route for Duy + Submit Homwork api

---
+++ .history/src/redux/modules/SubmitHomework/constants_20211224083006.js
@@ -0,0 +1,10 @@
+export const SUBMI_REQUEST = "homeworkReducer/SUBMI_REQUEST";
+export const SUBMI_SUCCESS = "homeworkReducer/SUBMI_SUCCESS";
+export const SUBMI_FAILED = "homeworkReducer/SUBMI_FAILED";
+export const SUBMI_RESET = "homeworkReducer/SUBMI_RESET";
+export const SUBMI_SEARCH = "homeworkReducer/SUBMI_SEARCH";
+
+export const SUBMI_DETAIL_REQUEST = "homeworkDetailReducer/SUBMI_DETAIL_REQUEST";
+export const SUBMI_DETAIL_SUCCESS = "homeworkDetailReducer/SUBMI_DETAIL_SUCCESS";
+export const SUBMI_DETAIL_FAILED = "homeworkDetailReducer/SUBMI_DETAIL_FAILED";
+export const SUBMI_DETAIL_RESET = "homeworkDetailReducer/SUBMI_DETAIL_RESET";
