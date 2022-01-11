eb4a100219c697de68d547c50d791f63e5a19abd
hienvi16 <hienvi162001@gmail.com>
2022-01-11 17:32:48 +0700

    fix warning

--- src/containers/HomeTemplate/TodoList.jsx
+++ src/containers/HomeTemplate/TodoList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 import Card from "@mui/material/Card";
