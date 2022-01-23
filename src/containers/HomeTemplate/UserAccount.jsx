a0d8facaba346e7066470e75c3c81a605b6eb8d7
Võ Hoàng Vũ <vuprotk21@gmail.com>
2022-01-23 13:57:40 +0700

    render avatar and user info

--- src/containers/HomeTemplate/UserAccount.jsx
+++ src/containers/HomeTemplate/UserAccount.jsx
@@ -6,9 +6,11 @@ import EditIcon from "@mui/icons-material/Edit";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserInfo } from "../../redux/modules/Home/action";
 import Loading from "../../components/Loading";
+import { Input } from "@mui/material";
 
 function UserAccount() {
   const dispatch = useDispatch();
+  const [render, setRender] = useState(false);
 
   useEffect(() => {
     dispatch(fetchUserInfo());
@@ -27,7 +29,12 @@ function UserAccount() {
 
   const user = useSelector((state) => state.fetchUserInfoReducer?.data?.user);
   const loading = useSelector((state) => state.fetchUserInfoReducer.loading);
-  const err = useSelector((state) => state.fetchUserInfoReducer.err);
+  //const err = useSelector((state) => state.fetchUserInfoReducer.err);
+  let avatar;
+  if (localStorage.getItem("avatar")) {
+    avatar = localStorage.getItem("avatar");
+  }
+
   const convertDate = (date) => {
     date = new Date(date);
     var dd = String(date.getDate()).padStart(2, "0");
@@ -38,19 +45,39 @@ function UserAccount() {
   if (loading) {
     return <Loading />;
   }
+  const onFilesChange = (files) => {};
 
   return (
     <div className="container">
       {/* <NavbarHome /> */}
 
       <div className="user-account ">
-        <img
-          src={pathImgFromIndex + "meo_ngu_ngoc.jpg"}
-          alt="avatar"
-          width="150"
-          height="150"
-          className="avatar"
-        ></img>
+        <div className="avatar" title="Change Avatar" id="change-avatar">
+          <input
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+            style={{ display: "none" }}
+            onChange={onFilesChange}
+          />
+          <label
+            htmlFor="contained-button-file"
+            style={{
+              cursor: "pointer",
+            }}
+          >
+            <img
+              src={
+                avatar !== null ? avatar : pathImgFromIndex + "meo_ngu_ngoc.jpg"
+              }
+              alt="avatar"
+              width="150"
+              height="150"
+              className="avatar"
+            ></img>
+          </label>
+        </div>
         <div className="info">
           <h2 className="username">{user?.username}</h2>
           <hr className="shareHr" />
@@ -62,6 +89,8 @@ function UserAccount() {
               <h4 className="field">{initialField.phone}</h4>
               <h4 className="field">{initialField.dayCreated}</h4>
               <h4 className="field">{initialField.numsClass}</h4>
+              <h4 className="field">{initialField.numsTeacher}</h4>
+              <h4 className="field">{initialField.numsStudent}</h4>
             </div>
             <div className="value-container">
               <h4 className="value-field">{user?.fullName}</h4>
